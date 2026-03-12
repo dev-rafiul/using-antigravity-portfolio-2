a12a17810ebfe03afa3b9fa37d088c3721497b50
COMRADE040 <rafiulislam040@gmail.com>
2026-03-12 23:31:30 +0600

    remove Education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-
 
 const Education = () => {
   return (
-    <section id="education" className="py-20 bg-white dark:bg-dark-bg transition-colors duration-300">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-4xl">
-        
-
-      </div>
-    </section>
+    <div>
+      
+    </div>
   );
 };
 
