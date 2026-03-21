ea630887dcbc323fd44ac72afd73793501af3401
COMRADE040 <rafiulislam040@gmail.com>
2026-03-21 23:49:52 +0600

    added about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,12 @@ import Projects from '../components/Projects';
 import Experience from '../components/Experience';
 import Education from '../components/Education';
 import Contact from '../components/Contact';
+import Header from '../components/Header';
 
 const Home = () => {
   return (
     <main>
+      
       <Hero></Hero>
       <About />
       <Skills />
