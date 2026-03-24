92c3d8fe6a54fa63751730898be6e6ba72d90433
COMRADE040 <rafiulislam040@gmail.com>
2026-03-24 23:14:59 +0600

    changed github url

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const contactInfo = [
 ];
 
 const socials = [
-  { icon:'🐙', label:'GitHub',   href:'https://github.com',   color:'#eef4f7' },
+  { icon:'🐙', label:'GitHub',   href:'https://github.com/dev-rafiul',   color:'#eef4f7' },
   { icon:'💼', label:'LinkedIn', href:'https://linkedin.com', color:'#0ea5e9' },
   { icon:'🐦', label:'Twitter',  href:'https://twitter.com',  color:'#38bdf8' },
   { icon:'📘', label:'Facebook', href:'https://facebook.com', color:'#3b82f6' },
