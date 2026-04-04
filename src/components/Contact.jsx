dcee9c9808e91c6f51e77d5ee80c05c392929377
COMRADE040 <rafiulislam040@gmail.com>
2026-04-04 23:31:06 +0600

    remove comment

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const socials = [
   { icon:'📘', label:'Facebook', href:'https://www.facebook.com/devrafiul0/', color:'#3b82f6' },
 ];
 
-/*  */
+
 function Particle({ color, style }) {
   return (
     <div style={{
